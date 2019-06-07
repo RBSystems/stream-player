f67b97ce49ec6c31026ebc76826f6c2d5ab2fbc7
Joe <joseph.as.peterson@gmail.com>
2019-06-07 15:05:11 -0600

    Added CAS middleware

--- server.go
+++ server.go
@@ -3,24 +3,17 @@ package main
 import (
 	"net/http"
 
-	"github.com/byuoitav/common"
 	"github.com/byuoitav/common/v2/auth"
 	"github.com/labstack/echo"
-	"github.com/labstack/echo/middleware"
 )
 
 func main() {
 	port := ":10001"
-	router := common.NewRouter()
 
-	router.Group("/",
-		echo.WrapMiddleware(auth.AuthenticateCASUser),
-		middleware.StaticWithConfig(middleware.StaticConfig{
-			Root:   "test",
-			Index:  "index.html",
-			HTML5:  true,
-			Browse: true,
-		}))
+	router := echo.New()
+	router.Use(echo.WrapMiddleware(auth.AuthenticateCASUser))
+
+	router.Static("/", "av-netflix-dist")
 
 	server := http.Server{
 		Addr:           port,
@@ -28,16 +21,4 @@ func main() {
 	}
 
 	router.StartServer(&server)
-
 }
-
-// router.Group("/", auth.CheckHeaderBasedAuth,
-// 		auth.CheckHeaderBasedAuth,
-// 		echo.WrapMiddleware(auth.AuthenticateCASUser),
-// 		auth.AuthorizeRequest("read-config", "configuration", func(c echo.Context) string { return "all" }),
-// 		middleware.StaticWithConfig(middleware.StaticConfig{
-// 			Root:   "web-dist",
-// 			Index:  "index.html",
-// 			HTML5:  true,
-// 			Browse: true,
-// 		}))
